13b314ffc5d3f324c875350b30eed7736a97a94e
Ondřej Hošák <ondrej.hosak@avg.com>
2016-06-24 17:53:25 +0200

    refactor: rewrite test result computing

--- lib/test_aggregation/build_result.rb
+++ lib/test_aggregation/build_result.rb
@@ -2,64 +2,25 @@ require 'set'
 require 'test_aggregation/class_result'
 
 module TestAggregation
+  # class representing build
   class BuildResults
     attr_reader :build
     attr_reader :parts, :jobs_by_parts_aggregations, :step_result_callback
 
-    class << self
-      def default_sum_results(results)
-        r = results.reject { |_res, count| count <= 0 }.keys.uniq
-
-        # when 'errored' step exists
-        return 'errored' if r.include?('errored')
-
-        # when 'failed' step exists
-        return 'failed' if r.include?('failed')
-
-        # when all are 'created', then created
-        # when all are 'pending', then pending
-        return r.first if r.size == 1 && StepResult::RESULTS.include?(r.first)
-
-        return 'passed' if
-          r.include?('passed') &&
-          (r - %w(passed pending skipped)).empty?
-
-        # when 'created' exists, e.g. test is still running
-        return 'created' if r.include?('created')
-
-        # when no results
-        return 'errored' if r.empty?
-
-        fail "Unknown result for: #{r.inspect}"
-      end
-
-      def sum_results(results)
-        @sum_results_method ||= method(:default_sum_results)
-        @sum_results_method.call(results)
-      end
-
-      def sum_results=(default_sum_result_method)
-        @sum_results_method = default_sum_result_method
-      end
-    end
-
     def initialize(build,
                    job_part_callback,
                    job_aggregate_callback,
-                   step_serialization_callback,
-                   step_result_callback = ->(step) {
-                     step[:result] && step[:result].downcase
-                   }
+                   step_status_rewrite_callback
                   )
       @build = build
 
       @job_part_callback = job_part_callback
       @job_aggregate_callback = job_aggregate_callback
-      @step_result_callback = step_result_callback
-      @step_serialization_callback = step_serialization_callback
+      @step_status_rewrite_callback = step_status_rewrite_callback
 
       @parts = {}
       @jobs_by_parts_aggregations = {}
+      @jobs_by_parts = {}
     end
 
     def aggregate_by(job)
@@ -70,8 +31,8 @@ module TestAggregation
       @job_part_callback[job]
     end
 
-    def step_result_constructor(step_result)
-      @step_serialization_callback[step_result]
+    def step_status_rewrite_callback(step_result)
+      @step_status_rewrite_callback[step_result]
     end
 
     def find_job(job_id)
@@ -80,9 +41,11 @@ module TestAggregation
 
     def parse(step)
       step_job_id = step['job_id']
-      fail 'Step job_id property is not specified' unless step_job_id
+      raise 'Step job_id property is not specified' unless step_job_id
       job = find_job(step_job_id)
-      fail "Could not find job for specified job_id:#{step_job_id}" unless job
+      raise "Could not find job for specified job_id:#{step_job_id}" unless job
+      class_position = step['class_position']
+      raise "Class position is undefined. Cannot parse step: #{step.inspect}" unless class_position
 
       part_name = part_by(job)
       aggregate_name = aggregate_by(job)
@@ -90,22 +53,60 @@ module TestAggregation
       @parts[part_name] ||= []
 
       test_cases = parts[part_name]
+
       @jobs_by_parts_aggregations[part_name] ||= {}
-      @jobs_by_parts_aggregations[part_name][aggregate_name] ||= Set.new
+      @jobs_by_parts_aggregations[part_name][aggregate_name] ||= []
       @jobs_by_parts_aggregations[part_name][aggregate_name] << job
 
-      class_position = step['class_position']
-      unless class_position
-        fail "Class position is undefined. Cannot parse step: #{step.inspect}"
-      end
+      @jobs_by_parts[part_name] ||= []
+      @jobs_by_parts[part_name] << job
+
+      # zero indexing
       class_position = class_position.to_i - 1
 
       test_cases[class_position] ||= ClassResult.new(self)
       test_cases[class_position].parse(step)
     end
 
-    def result(opts = {})
-      self.class.sum_results(results_hash(opts))
+    def test_case_result(results)
+      r = results.reject { |_res, count| count <= 0 }.keys.uniq.compact.map(&:to_s)
+
+      return 'Failed' if r.empty?
+
+      return 'Failed' if r.include?('failed') || r.include?('blocked')
+
+      return 'Passed' if r.all? { |result| ['passed', 'pending'].include?(result) }
+
+      return 'Loading' if r.include?('created')
+
+      TestAggregation.logger.error "Unknown result for: #{r.inspect}"
+      'Errored'
+    end
+
+    def part_result(part_name)
+      jobs = @jobs_by_parts[part_name]
+      return 'Created' unless jobs
+
+      job_results = jobs.map(&:state).uniq
+
+      return 'Failed' if job_results.include?('failed') || job_results.include?('canceled')
+      return 'Created' if job_results.include?('created')
+
+      'Passed'
+    end
+
+    def machine_result(results)
+      return 'Passed' if results.all? { |r| ['Passed', 'Skipped', 'NotPerformed'].include?(r) }
+
+      return 'Failed' if results.include? 'Failed'
+      return 'Skipped' if results.include? 'Skipped'
+      return 'Invalid' if results.include? 'Invalid'
+      return 'NotTested' if results.include? 'NotTested'
+      return 'KnownBug' if results.include? 'KnownBug'
+      return 'NotSet' if results.include? 'NotSet'
+      return 'Passed' if results.include? 'Passed'
+
+      'NotPerformed'
     end
 
     def results_hash(opts = {})
@@ -113,13 +114,33 @@ module TestAggregation
         next if opts[:part] && opts[:part] != part
         test_cases.each do |test_case|
           next unless test_case
-          test_case.results_hash(opts).each do |state_name, state_value|
+          test_case.class_results_hash(opts).each do |state_name, state_value|
             obj[state_name] ||= 0
             obj[state_name] += state_value
           end
         end
       end
       return { 'created' => 1 } if res.empty?
+      res
+    end
+
+    # this method return hash with rewrited states from new to old ones
+    # it is used for evaluating machine result that is evaluated from old states
+    def new_states_results_hash
+      res = parts.each_with_object({}) do |(_part, test_cases), obj|
+        test_cases.each do |test_case|
+          next unless test_case
+          test_case.test_steps.each do |test_step|
+            test_step.results.each do |(_machine, value)|
+              step_rewrited_name = @step_status_rewrite_callback.call(value)
+              obj[step_rewrited_name] ||= 0
+              obj[step_rewrited_name] += 1
+            end
+          end
+        end
+      end
+      return { 'NotSet' => 1 } if res.empty?
+
       res
     end
 
@@ -135,8 +156,9 @@ module TestAggregation
       test_cases = parts[part_name]
       {
         name: part_name,
-        result: result(part: part_name),
+        result: part_result(part_name),
         machines: jobs_by_parts_aggregations[part_name].keys.map do |machine_name|
+          puts machine_name
           machines_as_json(part_name, machine_name)
         end,
         testCases: test_cases.map do |test_case|
@@ -155,7 +177,7 @@ module TestAggregation
                end # What to do when zero or more jobs is defined?
       {
         os: machine_name,
-        result: result(part: part_name, machine: machine_name),
+        result: jobs.first.state == 'canceled' ? 'Failed' : jobs.first.state.capitalize,
         id: job_id
       }
     end
