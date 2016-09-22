657880febba459f11c6792a078d2a63e80847f00
Ondřej Hošák <ondrej.hosak@avg.com>
2016-09-22 17:39:38 +0200

    WIP2

--- lib/test_aggregation/class_result.rb
+++ lib/test_aggregation/class_result.rb
@@ -17,7 +17,7 @@ module TestAggregation
   class ClassResult
     attr_reader :build_result
     attr_reader :test_steps
-    attr_reader :name
+    attr_reader :name, :position
 
     def initialize(build_result)
       @build_result = build_result
@@ -34,7 +34,7 @@ module TestAggregation
     end
 
     def class_results_hash(opts = {})
-      # append added steps to calculate proper ratio
+      # TODO: include added steps
       test_steps.each_with_object({}) do |step, obj|
         next unless step
 
@@ -63,6 +63,7 @@ module TestAggregation
       end
 
       @name ||= class_name
+      @position ||= step['class_position']
 
       step_position = step['position']
       raise "Step position is undefined. Cannot parse step: #{step.inspect}" unless step_position
@@ -80,30 +81,65 @@ module TestAggregation
         test_step.as_json
       end
 
-      job_ids = @added_steps.map{ |as| as['job_id'] }.uniq
+      preprocessed_steps = preprocess_steps
 
-      preprocessed_steps = {}
+      begin
+        sorted_step_names = preprocessed_steps.tsort
 
-      # added steps data preprocess:
-      # we need to take only names and generate ordered list
-      # data are stored in this structure
-      # Example:
-      # machineA = ['stepA', 'stepB', 'stepC']
-      # machineB = ['stepD''stepE', 'stepC']
-      # preprocessed data: [
-      #   'stepA' => [],
-      #   'stepB' => ['stepA'],
-      #   'stepC' => ['stepA', 'stepB', 'stepD', 'stepE'],
-      #   'stepD' => [],
-      #   'stepE' => ['stepD'],
-      # ]
-      job_ids.each do |job_id|
+        # here is the tricky part, we can assamble the results
+        # or call good old parse method and compute the missing data
+
+        # for now this code assembles results
+
+        #normalized_steps += format_added_steps(sorted_step_names)
+        generate_data_for_cyclic_added_steps
+
+      rescue TSort::Cyclic
+        generate_data_for_cyclic_added_steps
+      end
+
+      @test_steps.map do |test_step|
+        next unless test_step
+        test_step.as_json
+      end
+
+      #normalized_steps
+    end
+
+    def as_json
+      {
+        description: name,
+        result: build_result.test_case_result(class_results_hash),
+        testSteps: normalized_test_steps
+      }
+    end
+
+    private
+
+    def job_ids
+      @added_steps.map{ |as| as['job_id'] }.uniq
+    end
+
+    # returns preprocess added step data:
+    # we need to take only names and generate ordered list
+    # data are stored in following structure
+    # Example:
+    # machineA = ['stepA', 'stepB', 'stepC']
+    # machineB = ['stepD''stepE', 'stepC']
+    # preprocessed data: [
+    #   'stepA' => [],
+    #   'stepB' => ['stepA'],
+    #   'stepC' => ['stepA', 'stepB', 'stepD', 'stepE'],
+    #   'stepD' => [],
+    #   'stepE' => ['stepD'],
+    # ]
+    def preprocess_steps
+      job_ids.each_with_object({}) do |job_id, preprocessed_hash_result|
         steps_by_job_id = @added_steps.find_all { |s| s['job_id'] == job_id }.sort_by { |s| s['position'] }
 
-        # filter by step result. We only want to store 'passed' insted of 'created' and 'passed'
-        # for each step
+        # filter by step result. We only want to store 'passed' insted of 'created' and 'passed' for each step
         steps_by_job_id.reject! do |sbji|
-          steps_by_job_id.find { |sxx| ['passed','failed'].include?(sxx['result']) } == nil
+          steps_by_job_id.find { |sxx| results_without_created.include?(sxx['result']) } != nil # TODO: really negation
         end
 
         # gather all step names in current job
@@ -113,31 +149,26 @@ module TestAggregation
 
         # generate data with dependency
         step_names.each_with_index do |ms, i|
-          preprocessed_steps[ms] ||= []
-          step_names.take(i).each { |name| preprocessed_steps[ms] << name }
+          preprocessed_hash_result[ms] ||= []
+          step_names.take(i).each { |name| preprocessed_hash_result[ms] << name }
         end
 
         # uniq the ancestors
-        preprocessed_steps.each { |_key, value| value.uniq! }
+        preprocessed_hash_result.each { |_key, value| value.uniq! }
       end
+    end
 
-      # apply sorting
-      sorted = []
-      begin
-        sorted = preprocessed_steps.tsort
-      rescue TSort::Cyclic
-        # TODO: process cyclic data
-      end
+    def format_added_steps(sorted)
+      int position = 1
 
-      # format data for final-api response
-      sorted.each do |s|
+      sorted.each_with_object([]) do |s, formated_added_steps|
         # all_states = results.each_with_object([]) do |(_machine, value), result|
         #   result << build_result.step_status_rewrite_callback(value)
         # end
 
         all_machines = {
           'all' => {
-            result: 'Passed',
+            result: 'Passed', # TODO: compute all machines result
             message: '',
             resultId: '00000000-0000-0000-0000-000000000000'
           }
@@ -163,22 +194,73 @@ module TestAggregation
 
         added_machines.merge(all_machines)
 
-        normalized_steps << {
+        formated_added_steps << {
           id: __id__, # temporary id until we can put here vCenter machine id
           description: s,
           machines: added_machines
         }
       end
-
-      normalized_steps
     end
 
-    def as_json
+    def generate_data_for_cyclic_added_steps
+      result_offset = 0
+
+      job_ids.each do |job_id|
+        steps_by_job_id = @added_steps.find_all { |s| s['job_id'] == job_id }.sort_by { |s| s['position'] }
+
+        # filter by step result. We only want to store 'passed' insted of 'created' and 'passed' for each step
+        puts '-'*80
+        puts results_without_created
+        puts '-'*80
+        puts steps_by_job_id
+
+        ## GROUP BY UUID TY MAKOVICEEEEEE
+        
+        steps_by_job_id.reject! do |sbji|
+          steps_by_job_id.find do |sxx|
+            results_without_created.include?(sxx['result']) && sxx['name'] == sbji['name']
+          end != nil
+        end
+
+        puts '-'*80
+        puts steps_by_job_id
+
+        steps_by_job_id.each do |sbji|
+          step_position = (sbji['position'] + result_offset) - 1 # zero indexing
+
+          job_ids.each do |generate_for_job_id|
+            mocked_step = not_performed_step(generate_for_job_id, sbji['name'], step_position)
+
+            @test_steps[step_position] ||= StepResult.new(self)
+
+            if generate_for_job_id != job_id
+              @test_steps[step_position].parse(mocked_step)
+            else
+              @test_steps[step_position].parse(sbji)
+            end
+          end
+
+          result_offset += 1
+        end
+      end
+    end
+
+    def not_performed_step(job_id, name, step_position)
       {
-        description: name,
-        result: build_result.test_case_result(class_results_hash),
-        testSteps: normalized_test_steps
+        'uuid' => '00000000-1001-1111-1001-000000000000',
+        'job_id' => job_id,
+        'name' => name,
+        #'position': step_position,
+        #'classname': name,
+        #'class_position': position,
+        'result' => 'pending',
+        'data'=> { 'status' => 'not_performed', 'imported' => true },
+        'number'=> 1
       }
+    end
+
+    def results_without_created
+      StepResult::RESULTS.reject { |a| a == 'created' }
     end
   end
 end
